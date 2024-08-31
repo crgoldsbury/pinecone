e7537785abee255a3f1a2e84eabbb9e6d4246cd3
Christopher R Goldsbury <crgoldsbury@gmail.com>
2024-08-31 16:24:28 -0500

    Initial outline of vision.

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+
+class PineCone
+
+    #default constructor.
+    def __init__(self):
+        self.array_of_seeds = []
+
+    # add a seed. a seed is a list data type.
+    def add_seed(self, seed):
+        self.array_of_seeds.append(seed)
+
+    #If the pinecone is populated with seeds, then it will not open. Only dispersion is allowed.
+    def open(self):
+
+    #once items are added this will encrypt everything and compress it. Only disperson is allowed post closing.
+    def close(self):
+
+
+    #disperse will spread seeds to intended target(s).
+    def disperse(self):
+
+
+
+
+
